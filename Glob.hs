f4898ef46922818de0804bce7730d4f8a5db3f4e
António <pg11024@gmail.com>
2019-02-06 21:00:06 +0000

    'regex'

--- Glob.hs
+++ Glob.hs
@@ -62,5 +62,16 @@ listPlain dirName baseName = do
               else doesNameExist (dirName </> baseName)
     return (if exists then [baseName] else [])
 
+renameWith :: (FilePath -> FilePath) -> FilePath -> IO FilePath
+renameWith f path = do
+    let path' = f path
+    rename path path'
+    return path'
 
+rename :: FilePath -> FilePath -> IO ()
+rename old new = do
+    isFile <- doesFileExist old
+    let f = if isFile then renameFile else renameDirectory
+    f old new
 
+cc2cpp = mapM (renameWith (flip replaceExtension ".cpp")) =<< namesMatching "*.cc"
